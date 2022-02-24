95cb6a50635a74d3ee43c008e73251150b4fcd67
ashton <ashtondmorris@gmail.com>
2022-02-24 08:40:12 -0700

    updated midi file - added a class export and added a setMidi function to download the midi file and load it into the player object.

--- src/midi.js
+++ src/midi.js
@@ -1,14 +1,69 @@
 import MidiPlayer from "midi-player-js";
-
-export function midi(url) {
-  let p = new MidiPlayer.Player((e) => {
-    console.log("url", url);
-    console.log("e", e);
-  });
-  try {
-    p.loadFile(url);
-  } catch (e) {
-    console.log("error", e);
+export class MyMidiPlayer {
+  constructor() {
+    // this._audioContext;
+    // this._safeAudioContext;
+    this._midi = null;
+    this._player = null;
   }
-  return p;
+
+  async setPlayer() {
+    // this._audioContext =
+    //   window.AudioContext || window.webkitAudioContext || false;
+    // this._safeAudioContext = new this._audioContext();
+    this._player = new MidiPlayer.Player((event) => {
+      // console.log(event);
+      if (event.name === "Controller Change") {
+        this.onControllerChange(event);
+      } else if (event.name === "Note on") {
+        this.onNoteOnEvent(event);
+      } else if (event.name === "Note off") {
+        this.onNoteOffEvent(event);
+      }
+    });
+  }
+
+  onControllerChange(event) {
+    // console.log("onControllerChange event", event);
+  }
+
+  onNoteOnEvent(event) {
+    // console.log("onNoteOnEvent event", event);
+  }
+
+  onNoteOffEvent(event) {
+    // console.log("onNoteOffEvent event", event);
+  }
+
+  async setMidi(midiUrl) {
+    // "../assets/chopin_etude_rev.mid"
+    this._midi = await fetch(midiUrl).then((response) =>
+      response.arrayBuffer()
+    );
+    console.log("this._midi", this._midi);
+    this._player.loadArrayBuffer(this._midi);
+  }
+
+  play() {
+    console.log(
+      "player?",
+      this._player.bytesProcessed(),
+      this._player.getSongTime(),
+      this._player.tracks
+    );
+    if (this._player.isPlaying()) {
+      console.log("stop");
+      this._player.stop();
+    }
+    console.log("play");
+    this._player.play();
+  }
+
+  stop() {
+    this._player.stop();
+  }
+
+  get player() {
+    return this._player;
+  }
 }
